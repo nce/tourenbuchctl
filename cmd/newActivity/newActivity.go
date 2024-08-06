4d67ae41a64ffc332cbd981dd3cb2895e78ef2c7
Ulli Goschler <git@nce.wtf>
2024-08-06 15:14:31 +0200

    add hike activity

--- cmd/newActivity/newActivity.go
+++ cmd/newActivity/newActivity.go
@@ -23,6 +23,7 @@ func NewNewCommand() *cobra.Command {
 		},
 	}
 	newCmd.AddCommand(newMtbCommand())
+	newCmd.AddCommand(newHikeCommand())
 	newCmd.AddCommand(newSkitourCommand())
 
 	return newCmd
@@ -68,7 +69,80 @@ func newMtbCommand() *cobra.Command {
 	cmd.Flags().StringVarP(&dateStr, "date", "d", "", "Date of the activity in the format 'DD.MM.YYYY'")
 	cmd.Flags().BoolVarP(&act.Meta.StravaSync, "sync", "s", true, "Get activity stats from strava")
 	cmd.Flags().BoolVarP(&act.Meta.StravaGpxSync, "gpx", "g", true, "Get gpx track from strava")
-	cmd.Flags().BoolVarP(&act.Meta.Multiday, "mutli", "m", false, "Is part of a multiday activity")
+	cmd.Flags().BoolVarP(&act.Meta.Multiday, "multi", "m", false, "Is part of a multiday activity")
+	cmd.Flags().BoolVarP(&act.Meta.QueryStartLocation, "start-location", "l", true, "Interactive"+
+		"query for starting locations")
+	cmd.Flags().IntVarP(&act.Tb.Rating, "rating", "r", 3, "Rating of the activity in the format '1-5'."+
+		"This will be later displayed as stars")
+	cmd.Flags().IntVarP(&act.Tb.Difficulty, "difficulty", "y", 3, "Difficulty of trails in S-Scale")
+
+	err := cmd.MarkFlagRequired("date")
+	if err != nil {
+		log.Fatal().Err(err).Msg("Error in marking flag as required")
+	}
+
+	err = cmd.MarkFlagRequired("title")
+	if err != nil {
+		log.Fatal().Err(err).Msg("Error in marking flag as required")
+	}
+
+	//nolint: revive
+	cmd.PreRunE = func(cmd *cobra.Command, args []string) error {
+		if dateStr != "" {
+			parsedDate, err := time.Parse("02.01.2006", dateStr)
+			if err != nil {
+				return fmt.Errorf("invalid date format: %w", err)
+			}
+
+			act.Tb.Date = parsedDate
+		}
+
+		return nil
+	}
+
+	return cmd
+}
+
+func newHikeCommand() *cobra.Command {
+	act := activity.Activity{}
+
+	cmd := &cobra.Command{
+		Use:   "hike [name]",
+		Short: "Create a new hike activity in Tourenbuch",
+		Long:  "Create a new hike activity",
+		Args:  cobra.ExactArgs(1),
+		//nolint: revive
+		Run: func(cmd *cobra.Command, args []string) {
+			act.Meta.Name = args[0]
+			act.Meta.Category = "wandern"
+			log.Info().Msg("Creating new hike activity")
+
+			if act.Meta.QueryStartLocation {
+				var err error
+				act.Tb.StartLocationQr, err = activity.GetStartLocationQr()
+				if err != nil {
+					log.Fatal().Err(err).Msg("Error getting start location")
+				}
+				log.Debug().Str("startLocationQr", act.Tb.StartLocationQr).Msg("Start location set")
+			}
+
+			err := act.CreateActivity()
+			if err != nil {
+				log.Fatal().Err(err).Msg("Error creating activity")
+			}
+		},
+	}
+
+	var dateStr string
+
+	//  cmd.Flags().IntVar(&act.Tb.MaxHeight, "height", "h", "Maximium absolute elevation in meter.")
+	cmd.Flags().StringVarP(&act.Tb.Title, "title", "t", "", "Title of the activity")
+	cmd.Flags().StringVarP(&act.Tb.Company, "company", "c", "", "Names of people who participated")
+	cmd.Flags().StringVar(&act.Tb.Restaurant, "restaurant", "", "Names of people who participated")
+	cmd.Flags().StringVarP(&dateStr, "date", "d", "", "Date of the activity in the format 'DD.MM.YYYY'")
+	cmd.Flags().BoolVarP(&act.Meta.StravaSync, "sync", "s", true, "Get activity stats from strava")
+	cmd.Flags().BoolVarP(&act.Meta.StravaGpxSync, "gpx", "g", true, "Get gpx track from strava")
+	cmd.Flags().BoolVarP(&act.Meta.Multiday, "multi", "m", false, "Is part of a multiday activity")
 	cmd.Flags().BoolVarP(&act.Meta.QueryStartLocation, "start-location", "l", true, "Interactive"+
 		"query for starting locations")
 	cmd.Flags().IntVarP(&act.Tb.Rating, "rating", "r", 3, "Rating of the activity in the format '1-5'."+
---
+++ templates/tourenbuch/wandern/elevation.plt
@@ -0,0 +1,77 @@
+# vim: set ft=gnuplot:
+# WANDERN VERSION
+# -- [ Colors ] --
+# Wandern   ForestGreen #009B55
+# Alternat. Olivegreen  #3C8031
+wandern     = "#009B55"
+olivegreen  = "#3C8031"
+
+mycolor = wandern
+altcolor = olivegreen
+
+# -- [ Scaling ] --
+YTICS = 100
+
+# WANDERN
+wandernKM = 44.97   # 17.99cm / 40km
+wandernHM = 0.300
+
+myscaleKM = wandernKM
+myscaleHM = wandernHM
+# ---
+
+file = 'gpxdata.txt'
+
+reset
+unset key
+stats file u 3:2 nooutput
+set xrange [STATS_min_x:STATS_max_x]
+# floor to lowest elevation, to always display minimum elevation;
+set yrange [floor(STATS_min_y/YTICS) * YTICS:STATS_max_y]
+set y2range [floor(STATS_min_y/YTICS) * YTICS:STATS_max_y]
+
+# Max Km mit Skalierungsfaktor. Den (fiktiven) Pixelwert von 300dpi auf cm umrechnen und den gesamten Skalierungsfaktor darauf mulitiplizieren.
+# vielleicht nicht so ideal
+w = floor(STATS_max_x * myscaleKM) * 2.54 / 300 * 2
+# h = floor((STATS_max_y - STATS_min_y) * myscaleHM) * 2.54 / 300 * 2
+# switch from 300 to 250 as small tours are getting dense 07.2016
+h = floor((STATS_max_y - STATS_min_y) * myscaleHM) * 2.54 / 250 * 2
+
+# -- [ Labels ] --
+set label 1 '\textcolor{wandern}{TOP}' at STATS_pos_max_y,STATS_max_y point pointtype 7 ps 0.6 offset 0.3,0.3 front
+#set label 2 '\textcolor{wandern}{NNN}' at XXX,YYY point pointtype 7 ps 0.6 offset 0.3,0.3 front
+
+# -- [ Terminal ] --
+# epslates does not support transparency
+# for mac xquartz is required to install epslatex https://xquartz.macosforge.org/landing/
+# brew install gnuplot --cairo
+set terminal cairolatex size w cm,h cm color
+set output "elevation.tex"
+
+# -- [ Grid and Tics ] --
+set xtic 5
+set xtic nomirror
+unset border
+set grid lt 0 dashtype 2 lw 3 lc rgb "black"
+set format x "%g\\tiny\\,\\color{darkgray}{km}"
+
+# ELEVATION SCALE ON THE RIGHT:
+#set y2tics border 200
+#set format y2 "%g\\tiny\\,\\color{darkgray}{m}"
+# OR ON THE LEFT:
+set ytics border YTICS nomirror
+set format y "%h\\tiny\\,\\color{darkgray}{m}"
+# cut right margin
+set rmargin at screen 1
+
+# ---
+# plot border: 1 bottom; 2 left
+set border 3 lt 3 lw 3 lc rgb "#708090"
+
+# correct the margin calculations which are based
+# on the length of the format string, to a fixed value
+set lmargin 4.8
+
+set style fill transparent solid 0.45 noborder
+plot file u 3:2 w filledcurve x1 lc rgb "black" , \
+  file u 3:2 w lines lt 1 lw 5 lc rgb mycolor
