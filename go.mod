531f805b743bca3f12f8e86f380fe2e906fef8bb
Ulli Goschler <git@nce.wtf>
2024-10-08 10:07:00 +0200

    utilize yaml struct to update/insert version number (#19)

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/rs/zerolog v1.33.0
 	github.com/spf13/cobra v1.8.1
 	github.com/spf13/viper v1.19.0
+	github.com/stretchr/testify v1.9.0
 	github.com/tkrajina/gpxgo v1.4.0
 	golang.org/x/oauth2 v0.21.0
 	golang.org/x/text v0.14.0
@@ -14,6 +15,7 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
@@ -22,6 +24,7 @@ require (
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
--- pkg/migrate/updateversion.go
+++ pkg/migrate/updateversion.go
@@ -1,129 +1,43 @@
 package migrate
 
 import (
-	"bufio"
+	"bytes"
 	"fmt"
 	"os"
-	"slices"
-	"strings"
+
+	"github.com/nce/tourenbuchctl/pkg/activity"
+	"gopkg.in/yaml.v3"
 )
 
-func normalizeHeaderFile(fileLocation string) error {
-	if err := removeLinesFromFile(fileLocation + "header.yaml"); err != nil {
-		return fmt.Errorf("error removing lines from header.yaml: %w", err)
-	}
-
-	return nil
-}
-
-func updateVersion(filename, newVersion string) error {
-	originalContent, err := os.ReadFile(filename)
-	if err != nil {
-		return fmt.Errorf("error reading file: %w", err)
-	}
-
-	lines := strings.Split(string(originalContent), "\n")
-
-	for i, line := range lines {
-		if strings.Contains(line, "  version:") {
-			lines[i] = "  version: " + newVersion
-		}
-	}
-
-	updatedContent := strings.Join(lines, "\n")
-
-	// Write the updated content back to the file
-	//nolint: gosec
-	err = os.WriteFile(filename, []byte(updatedContent), 0o644)
-	if err != nil {
-		return fmt.Errorf("error writing file: %w", err)
-	}
-
-	return nil
-}
-
-func removeLinesFromFile(filePath string) error {
-	linesToRemove := []string{
-		"# vim: set filetype=yaml:",
-		"---",
-		"# default",
-		"# adjust if excess of space",
-	}
-
-	inputFile, err := os.Open(filePath)
-	if err != nil {
-		return fmt.Errorf("error opening file: %w", err)
-	}
-	defer inputFile.Close()
-
-	tempFile, err := os.CreateTemp("", "tempfile")
-	if err != nil {
-		return fmt.Errorf("error creating temp file: %w", err)
-	}
-	defer tempFile.Close()
-
-	// Read the input file line by line and write to the temp file
-	scanner := bufio.NewScanner(inputFile)
-	writer := bufio.NewWriter(tempFile)
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		if !slices.Contains(linesToRemove, strings.TrimSpace(line)) {
-			_, err := writer.WriteString(line + "\n")
-			if err != nil {
-				return fmt.Errorf("error writing to temp file: %w", err)
-			}
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		return fmt.Errorf("error reading file: %w", err)
-	}
-
-	writer.Flush()
-
-	// Close both files
-	inputFile.Close()
-	tempFile.Close()
-
-	// Replace the original file with the temp file
-	err = os.Rename(tempFile.Name(), filePath)
-	if err != nil {
-		return fmt.Errorf("error replacing file: %w", err)
-	}
-
-	return nil
-}
-
 func InsertOrUpdateVersion(filePath string, version string) (bool, error) {
-	versionLine := "meta:\n  version:"
-
-	if err := normalizeHeaderFile(filePath); err != nil {
-		return false, fmt.Errorf("error normalizingHeader: %w", err)
-	}
-
-	originalFile, err := os.ReadFile(filePath + "header.yaml")
+	data, err := os.ReadFile(filePath + "/header.yaml")
 	if err != nil {
 		return false, fmt.Errorf("error reading file: %w", err)
 	}
 
-	if strings.HasPrefix(string(originalFile), versionLine) {
-		err := updateVersion(filePath+"header.yaml", version)
-		if err != nil {
-			return false, fmt.Errorf("error setting new version on header.yaml: %w", err)
-		}
+	var header activity.Header
 
-		return true, nil
+	err = yaml.Unmarshal(data, &header)
+	if err != nil {
+		return false, fmt.Errorf("error unmarshalling YAML: %w", err)
 	}
 
-	// Prepend the new content to the original content
-	newContent := []byte(versionLine + " " + version + "\n" + string(originalFile))
+	header.Meta.Version = version
 
-	// Write the updated content back to the file
-	//nolint: gosec
-	err = os.WriteFile(filePath+"header.yaml", newContent, 0o644)
+	// Marshal the updated data back to YAML
+	var enc bytes.Buffer
+	yamlEncoder := yaml.NewEncoder(&enc)
+	yamlEncoder.SetIndent(2)
+
+	err = yamlEncoder.Encode(&header)
 	if err != nil {
-		return false, fmt.Errorf("error writing file: %w", err)
+		return false, fmt.Errorf("error encoding YAML: %w", err)
+	}
+
+	//nolint: gosec
+	err = os.WriteFile(filePath+"/header.yaml", enc.Bytes(), 0o644)
+	if err != nil {
+		return false, fmt.Errorf("error writing to file: %w", err)
 	}
 
 	return true, nil
