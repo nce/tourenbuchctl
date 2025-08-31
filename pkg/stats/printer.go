6d49ca81576da14516b1b8cfc2921bd149f76f9c
Ulli Goschler <git@nce.wtf>
2025-08-31 20:32:01 +0200

    fix: rework region specific grouping

--- pkg/stats/printer.go
+++ pkg/stats/printer.go
@@ -11,32 +11,47 @@ import (
 func printMarkdown(activityCollection []activityData, regionalGrouping bool) {
 	doc := md.NewMarkdown(os.Stdout)
 
-	doc = doc.H1("MTB")
+	var tableContent [][]string
 
-	regions := uniqueRegions(activityCollection)
-	for _, region := range regions {
-		var tableContent [][]string
+	if regionalGrouping {
+		regions := uniqueRegions(activityCollection)
+		for _, region := range regions {
+			doc = doc.H2(region.Region)
 
-		doc = doc.H2(region.Region)
-
-		for _, activity := range activityCollection {
-			if activity.Region == region.Region {
-				line := []string{
-					activity.Title, activity.Region, activity.Ascent, activity.Distance, activity.Duration,
+			for _, activity := range activityCollection {
+				if activity.Region == region.Region {
+					line := []string{
+						"[" + activity.Title + "](" + activity.Dirname + ")" + " (" + activity.Date + ")",
+						activity.Ascent + "hm",
+						activity.Distance + "km",
+						activity.Duration + "h",
+						activity.Participants,
+					}
+					tableContent = append(tableContent, line)
 				}
-				tableContent = append(tableContent, line)
 			}
 		}
-
-		sort.Slice(tableContent, func(i, j int) bool {
-			return tableContent[i][0] < tableContent[j][0]
-		})
-
-		doc = doc.Table(md.TableSet{
-			Header: []string{"Name", "Region", "Ascent", "Distance", "Duration"},
-			Rows:   tableContent,
-		})
+	} else {
+		for _, activity := range activityCollection {
+			line := []string{
+				"[" + activity.Title + "](" + activity.Dirname + ")" + " (" + activity.Date + ")",
+				activity.Ascent + "hm",
+				activity.Distance + "km",
+				activity.Duration + "h",
+				activity.Participants,
+			}
+			tableContent = append(tableContent, line)
+		}
 	}
+
+	sort.Slice(tableContent, func(i, j int) bool {
+		return tableContent[i][0] < tableContent[j][0]
+	})
+
+	doc = doc.Table(md.TableSet{
+		Header: []string{"Name", "Ascent", "Distance", "Duration", "Participants"},
+		Rows:   tableContent,
+	})
 
 	err := doc.Build()
 	if err != nil {
